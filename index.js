71ed9bde7458046bdbe7e00db5972f06abc694ab
Kim Young Dae <38598117+zer0big@users.noreply.github.com>
2020-01-08 14:24:57 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Node World!");
+    response.end("Hello N0de World!");
 });
 
 const port = process.env.PORT || 1337;
